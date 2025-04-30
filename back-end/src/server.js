491cdec324b43bb62bbc6d0d95db10a8c7ca92bb
shriira-it <admin@shriira.one>
2025-04-30 15:05:12 +0530

    production

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 import { MongoClient, ReturnDocument, ServerApiVersion } from "mongodb";
 import admin from "firebase-admin";
 import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
 
@@ -34,6 +40,12 @@ async function connectToDB() {
   db = client.db("full-stack-react-db");
 }
 
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
 
@@ -52,7 +64,6 @@ app.use(async function (req, res, next) {
   } else {
     res.sendStatus(400);
   }
-
 });
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
@@ -96,11 +107,13 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   res.json(updatedArticle);
 });
 
+const PORT = process.env.PORT || 8000;
+
 async function start() {
   await connectToDB();
 
-  app.listen(8000, function () {
-    console.log("Server is listening on port 8000");
+  app.listen(PORT, function () {
+    console.log("Server is listening on port " + PORT);
   });
 }
 
